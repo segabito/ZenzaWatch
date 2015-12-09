8c09c4a3497b3d1b228afea0827b5329861ed2c5
segabito <segabito+gravatar@gmail.com>
2015-12-10 03:09:50 +0900

    バージョン番号上げ忘れ

--- ZenzaWatch.user.js
+++ ZenzaWatch.user.js
@@ -6,7 +6,7 @@
 // @match          http://ext.nicovideo.jp/*
 // @grant          none
 // @author         segabito macmoto
-// @version        0.1.12
+// @version        0.1.13
 // @require        https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js
 // ==/UserScript==
 
