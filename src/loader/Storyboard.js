a6f4c75abadd8e374b2942ca045180c68b60c7c4
segabito <segabito+gravatar@gmail.com>
2017-08-08 01:47:33 +0900

    Storyboardコミット漏れ

---
+++ src/loader/Storyboard.js
@@ -0,0 +1,299 @@
+const _ = require('lodash');
+const ajax = function() {};
+
+const ZenzaWatch = {
+  init: {
+  },
+  api: {}
+};
+const util = {};
+const location = {
+  protocol: 'http:'
+};
+
+
+//===BEGIN===
+
+    const SmileStoryboardInfoLoader = (function() {
+      var reject = function(err) {
+        return new Promise(function(res, rej) {
+          window.setTimeout(function() { rej(err); }, 0);
+        });
+      };
+
+      var parseStoryboard = function($storyboard, url) {
+        var storyboardId = $storyboard.attr('id') || '1';
+        return {
+          id:       storyboardId,
+          url:      url.replace('sb=1', 'sb=' + storyboardId),
+          thumbnail:{
+            width:    $storyboard.find('thumbnail_width').text(),
+            height:   $storyboard.find('thumbnail_height').text(),
+            number:   $storyboard.find('thumbnail_number').text(),
+            interval: $storyboard.find('thumbnail_interval').text()
+          },
+          board: {
+            rows:   $storyboard.find('board_rows').text(),
+            cols:   $storyboard.find('board_cols').text(),
+            number: $storyboard.find('board_number').text()
+          }
+        };
+      };
+
+      var parseXml = function(xml, url) {
+        var $xml = $(xml), $storyboard = $xml.find('storyboard');
+
+        if ($storyboard.length < 1) {
+          return null;
+        }
+
+        var info = {
+          format: 'smile',
+          status:   'ok',
+          message:  '成功',
+          url:      url,
+          movieId:  $xml.find('movie').attr('id'),
+          duration: $xml.find('duration').text(),
+          storyboard: []
+        };
+
+        for (var i = 0, len = $storyboard.length; i < len; i++) {
+          var sbInfo = parseStoryboard($($storyboard[i]), url);
+          info.storyboard.push(sbInfo);
+        }
+        info.storyboard.sort(function(a, b) {
+          var idA = parseInt(a.id.substr(1), 10), idB = parseInt(b.id.substr(1), 10);
+          return (idA < idB) ? 1 : -1;
+        });
+        return info;
+      };
+
+
+      var load = function(videoFileUrl) {
+        var a = document.createElement('a');
+        a.href = videoFileUrl;
+        var server = a.host;
+        var search = a.search;
+
+        if (!/\?(.)=(\d+)\.(\d+)/.test(search)) {
+          return reject({status: 'fail', message: 'invalid url', url: videoFileUrl});
+        }
+
+        var fileType = RegExp.$1;
+        var fileId   = RegExp.$2;
+        var key      = RegExp.$3;
+
+        if (fileType !== 'm') {
+          return reject({status: 'fail', message: 'unknown file type', url: videoFileUrl});
+        }
+
+        return new Promise(function(resolve, reject) {
+          var url = '//' + server + '/smile?m=' + fileId + '.' + key + '&sb=1';
+
+          util.fetch(url, {credentials: 'include'})
+            .then(res => { return res.text(); })
+            .then(result => {
+            const info = parseXml(result, url);
+
+            if (info) {
+              resolve(info);
+            } else {
+              reject({
+                status: 'fail',
+                message: 'storyboard not exist (1)',
+                result: result,
+                url: url
+              });
+            }
+          }, (err) => {
+            reject({
+              status: 'fail',
+              message: 'storyboard not exist (2)',
+              result: err,
+              url: url
+            });
+          });
+        });
+      };
+
+      return {
+        load
+      };
+    })();
+    ZenzaWatch.api.SmileStoryboardInfoLoader = SmileStoryboardInfoLoader;
+
+    const DmcStoryboardInfoLoader = (() => {
+      const parseStoryboard = function(sb) {
+        const result = {
+          id: 0,
+          urls: [],
+          quality: sb.quality,
+          thumbnail:{
+            width:    sb.thumbnail_width,
+            height:   sb.thumbnail_height,
+            number:   null,
+            interval: sb.interval
+          },
+          board: {
+            rows:   sb.rows,
+            cols:   sb.columns,
+            number: sb.images.length
+          }
+        };
+        sb.images.forEach(image => {
+          result.urls.push(image.uri);
+        });
+
+        return result;
+      };
+
+
+      const parseMeta = function(meta) {
+        const result =  {
+          format: 'dmc',
+          status: meta.meta.message,
+          url: null,
+          movieId: null,
+          storyboard: []
+        };
+
+        meta.data.storyboards.forEach(sb => {
+          result.storyboard.unshift(parseStoryboard(sb));
+        });
+
+        // 画質の良い順にソート
+        result.storyboard.sort((a, b) => {
+          if(a.quality < b.quality) { return  1; }
+          if(a.quality > b.quality) { return -1; }
+          return 0;
+        });
+
+        return result;
+      };
+
+
+      const load = function(url) {
+        return util.fetch(url, {credentials: 'include'})
+          .then(res => { return res.json(); })
+          .then(info => {
+            if (!info.meta || !info.meta.message || info.meta.message !== 'ok') {
+              return Promise.reject('storyboard request fail');
+            }
+            return parseMeta(info);
+          });
+      };
+
+      return {
+        load,
+        _parseMeta: parseMeta,
+        _parseStoryboard: parseStoryboard
+      };
+    })();
+    ZenzaWatch.api.DmcStoryboardInfoLoader = DmcStoryboardInfoLoader;
+
+    const StoryboardInfoLoader = (() => {
+      return {
+        load: function(url) {
+          if (url.match(/dmc\.nico/)) {
+            return DmcStoryboardInfoLoader.load(url);
+          } else {
+            return SmileStoryboardInfoLoader.load(url);
+          }
+        }
+      };
+    })();
+    ZenzaWatch.api.StoryboardInfoLoader = StoryboardInfoLoader;
+
+    class StoryboardSession {
+      constructor(info) {
+        this._info = info;
+        this._url = info.urls[0].url;
+      }
+
+      create() {
+        const url = `${this._url}?_format=json`;
+        const body = this._createRequestString(this._info);
+        return util.fetch(url, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body
+        }).then(res => {
+          return res.json();
+        }).catch(err => {
+          window.console.error('create dmc session fail', err);
+          return Promise.reject('create dmc session fail');
+        });
+      }
+
+      _createRequestString(info) {
+        if (!info) { info = this._info; }
+
+        // 階層が深くて目が疲れた
+        const request = {
+          session:  {
+            client_info: {
+              player_id: info.player_id
+            },
+            content_auth: {
+              auth_type: info.auth_types.storyboard,
+              content_key_timeout: info.content_key_timeout,
+              service_id: 'nicovideo',
+              service_user_id: info.service_user_id
+            },
+            content_id: info.content_id,
+            content_src_id_sets: [{
+              content_src_ids: []
+            }],
+            content_type: 'video',
+            content_uri: '',
+            keep_method: {
+              heartbeat: {
+                lifetime: info.heartbeat_lifetime
+              }
+            },
+            priority: info.priority,
+            protocol: {
+              name: 'http',
+              parameters: {
+                http_parameters: {
+                  parameters: {
+                    storyboard_download_parameters: {
+                      use_well_known_port: info.urls[0].is_well_known_port ? 'yes' : 'no',
+                      use_ssl: info.urls[0].is_ssl ? 'yes' : 'no'
+                    }
+                  }
+                }
+              }
+            },
+            recipe_id: info.recipe_id,
+            session_operation_auth: {
+              session_operation_auth_by_signature: {
+                signature: info.signature,
+                token: info.token
+              }
+            },
+            timing_constraint: 'unlimited'
+          }
+        };
+
+        (info.videos || []).forEach(video => {
+          request.session.content_src_id_sets[0].content_src_ids.push(video);
+        });
+
+        //window.console.log('storyboard session request', JSON.stringify(request, null, ' '));
+        return JSON.stringify(request);
+      }
+    }
+//===END===
+//
+module.exports = {
+  SmileStoryboardInfoLoader,
+  DmcStoryboardInfoLoader,
+  StoryboardInfoLoader,
+  StoryboardSession
+};
+
+
---
+++ test/StoryboardTest.js
@@ -0,0 +1,146 @@
+const assert = require('power-assert');
+const {StoryboardSession, DmcStoryboardInfoLoader} = require('../src/loader/Storyboard');
+const sinon = require('sinon');
+
+
+describe('Storyboard apiのテスト', () => {
+
+  describe('リクエストデータ生成', () => {
+    let info = {
+      audios: [],
+      auth_types: {
+        storyboard: "auth-12345"
+      },
+      content_id: "12345",
+      content_key_timeout: Math.floor(Math.random() * 60000),
+      heartbeat_lifetime: Math.floor(Math.random() * 30000),
+      movies: [],
+      player_id: "abcdefg-h-ijklmnopqrstr_1234567890",
+      priority: Math.random(),
+      protocols: [
+        "niconico"
+      ],
+      recipe_id: "sushi-sm9",
+      service_user_id: '1234',
+      signature: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      token: "12345-token",
+      urls: [
+        {
+          is_ssl: Math.random() > 0.5,
+          is_well_known_port: Math.random() > 0.5,
+          url: '//example.com/api/sessions'
+        },
+        {
+          is_ssl: Math.random() > 0.5,
+          is_well_known_port: Math.random() > 0.5,
+          url: '//example.com/api/sessions'
+        }
+      ],
+      videos: [
+        '720p_aaaaa',
+        '540p_bbbbb'
+      ]
+    };
+
+    let session = new StoryboardSession(info);
+    let request = JSON.parse(session._createRequestString());
+
+    // 業が深い ネストが深い XMLで書くよりはましか
+
+    assert.equal(info.player_id, request.session.client_info.player_id);
+
+    assert.equal(info.auth_types.storyboard, request.session.content_auth.auth_type);
+    assert.equal(info.content_key_timeout,   request.session.content_auth.content_key_timeout);
+    assert.equal(info.service_user_id,       request.session.content_auth.service_user_id);
+
+    assert.equal(info.content_id, request.session.content_id);
+
+    assert.equal(info.heartbeat_lifetime, request.session.keep_method.heartbeat.lifetime);
+    
+    assert.equal(info.heartbeat_lifetime, request.session.keep_method.heartbeat.lifetime);
+
+    assert.equal(info.priority, request.session.priority);
+
+    assert.equal(info.urls[0].is_ssl             ? 'yes' : 'no',
+      request.session.protocol.parameters.http_parameters.parameters.storyboard_download_parameters.use_ssl);
+    assert.equal(info.urls[0].is_well_known_port ? 'yes' : 'no',
+      request.session.protocol.parameters.http_parameters.parameters.storyboard_download_parameters.use_well_known_port);
+
+    assert.equal(info.recipe_id, request.session.recipe_id);
+
+    assert.equal(info.signature,
+      request.session.session_operation_auth.session_operation_auth_by_signature.signature);
+    assert.equal(info.token,
+      request.session.session_operation_auth.session_operation_auth_by_signature.token);
+
+    assert.equal(info.videos[0], request.session.content_src_id_sets[0].content_src_ids[0]);
+    assert.equal(info.videos[1], request.session.content_src_id_sets[0].content_src_ids[1]);
+
+  });
+
+  const metadata = {
+     "meta": {
+      "status": 200,
+      "message": "ok"
+     },
+     "data": {
+      "version": 1,
+      "storyboards": [
+       {
+        "thumbnail_width": Math.floor(Math.random() * 320),
+        "thumbnail_height": Math.floor(Math.random() * 180),
+        "rows": Math.floor(Math.random() * 50),
+        "columns": 10,
+        "interval": 1000,
+        "quality": Math.floor(Math.random() * 100),
+        "images": [
+         {
+          "timestamp": 0,
+          "uri": "http://example.com/storyboard/ht2_nicovideo/nicovideo-smXXXXXX1"
+         },
+         {
+          "timestamp": 100000,
+          "uri": "http://example.com/storyboard/ht2_nicovideo/nicovideo-smXXXXXX2"
+         }
+        ]
+       },
+       {
+        "thumbnail_width": Math.floor(Math.random() * 320),
+        "thumbnail_height": Math.floor(Math.random() * 180),
+        "rows": Math.floor(Math.random() * 50),
+        "columns": 10,
+        "interval": 1000,
+        "quality": Math.floor(Math.random() * 100),
+        "images": [
+         {
+          "timestamp": 0,
+          "uri": "http://example.com/storyboard/ht2_nicovideo/nicovideo-smXXXXXX3"
+         },
+         {
+          "timestamp": 100000,
+          "uri": "http://example.com/storyboard/ht2_nicovideo/nicovideo-smXXXXXX4"
+         }
+        ]
+       }
+      ]
+    }
+  };
+
+  describe('メタデータのパース', () => {
+    const data = DmcStoryboardInfoLoader._parseMeta(metadata);
+    //console.log(JSON.stringify(data, null, ' '));
+    assert.equal(data.format, 'dmc');
+    assert.equal(data.status, 'ok');
+    assert.equal(data.url,     null);
+    assert.equal(data.movieId, null);
+
+    assert.equal(2, data.storyboard.length);
+    assert.equal(true,
+      data.storyboard[0].quality >= data.storyboard[1].quality,
+      '画質の良い順にソートされる'
+    );
+
+  });
+
+});
+
