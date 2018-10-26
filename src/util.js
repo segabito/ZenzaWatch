6574f49be455a869982dda26d81c1f1cddcb5287
m_p <maccha@fujiwarahaji.me>
2018-10-26 14:50:13 +0900

    動画のサムネイルが出ない問題を改善
    従来の計算方法ではSSLアクセス時に証明書エラーで画像が読み込めないので、「tn.smilevideo.jp」サーバーに固定しました。

--- src/util.js
+++ src/util.js
@@ -794,9 +794,8 @@ class CrossDomainGate {}
           return null;
         }
         const fileId = parseInt(videoId.substr(2), 10);
-        const num = (fileId % 4) + 1;
         const large = util.hasLargeThumbnail(videoId) ? '.L' : '';
-        return '//tn-skr' + num + '.smilevideo.jp/smile?i=' + fileId + large;
+        return '//tn.smilevideo.jp/smile?i=' + fileId + large;
       };
     })();
     ZenzaWatch.util.getThumbnailUrlByVideoId = getThumbnailUrlByVideoId;
